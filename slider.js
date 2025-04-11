52198e09be8e7e8a315e51965d0778eedbfc1bd1
Orlanstein <o.g.j.c051204@gmail.com>
2025-04-11 09:04:19 -0600

    Slider

---
+++ slider.js
@@ -0,0 +1,24 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.transform = `translateX(${100 * (i - index)}%)`;
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    showSlide(currentSlide);
+}
+
+// Initialize slider
+showSlide(currentSlide);
+
+setInterval(nextSlide, 5000);
